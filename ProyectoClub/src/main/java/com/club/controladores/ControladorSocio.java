eb56bd1a9a69ea3700aafe13a380ecb06082a42e
frlv73 <leovilla91@gmail.com>
2018-02-14 20:30:07 -0300

    Editar Socio

--- ProyectoClub/src/main/java/com/club/controladores/ControladorSocio.java
+++ ProyectoClub/src/main/java/com/club/controladores/ControladorSocio.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.club.entidades.Instalacion;
 import com.club.entidades.Socio;
+import com.club.servicios.ICategoriaServicio;
 import com.club.servicios.ISocioServicio;
 
 @Controller
@@ -17,6 +20,9 @@ public class ControladorSocio {
 
 	 @Autowired
 	private ISocioServicio servicio;
+	 @Autowired
+	private ICategoriaServicio servCat;
+	private Socio socio;
 	 
 	 @RequestMapping(method = RequestMethod.GET)
 		public String lista(ModelMap model) {
@@ -29,6 +35,35 @@ public class ControladorSocio {
 	public String registrar(@ModelAttribute("formSocio") Socio socio, ModelMap model) {
 		servicio.registrar(socio);
 		return "/login";
+	}
+
+	@RequestMapping(value = "/agregar", method = RequestMethod.GET)
+	public String agregar(ModelMap model) {
+
+		// TODO es necesario crear una nueva instalación o es mejor crearla cuando
+		// proceso el formulario?
+		socio = new Socio();
+		model.put("socio", socio);
+		model.put("titulo", "Nuevo Socio");
+		model.put("modo", "add");
+		return "socios/formSocio";
+	}
+
+	@RequestMapping(value = "/eliminar/{id}", method = RequestMethod.GET)
+	public String eliminar(@PathVariable("id") int id) {
+
+		servicio.eliminar(id);
+		return "socios/listaSocios";
+	}
+	
+	@RequestMapping(value = "/editar/{id}", method = RequestMethod.GET)
+	public String editar(@PathVariable("id") int idSocio, ModelMap model) {
+		socio = servicio.getSocioPorId(idSocio);
+		model.put("socio", socio);
+		model.put("titulo", "Modificar Socio");
+		model.put("listaCategorias", servCat.getAllCategorias());
+		model.put("modo", "edit");
+		return "socios/formSocio";
 	}
 
 }
